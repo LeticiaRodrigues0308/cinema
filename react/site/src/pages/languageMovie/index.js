af54b09fa1aa68245eef7e81c1310958c92e9e08
João <joaopedroapparecido@gmail.com>
2021-12-03 16:35:26 -0300

    Oi amigos

---
+++ react/site/src/pages/languageMovie/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container} from "./styled";
+
+export default function LanguageMovie(){
+    return(
+        <Container>
+        <div className="cabecalho">
+            <div className="logo"><img src="./assets/images/iconCinemonk.svg" alt=""/></div>
+            <div className="texto">Selecione o horário e o idioma</div>
+        </div>
+        
+        </Container>
+    );
+} 
