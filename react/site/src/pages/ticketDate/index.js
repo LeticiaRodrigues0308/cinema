e97542ef8a4150b65b900db2ec79e3b950e0d194
Laura Faustino <laurarfs18@gmail.com>
2021-12-03 11:15:08 -0300

    cabecalho tela de ticketDate

--- react/site/src/pages/ticketDate/index.js
+++ react/site/src/pages/ticketDate/index.js
@@ -1,8 +1,21 @@
-import { containerPage } from "./styled.js";
+import React from "react";
+import { Cabecalho, Conteiner, Texto } from "./styled"
 
 
 export default function ticketDate() {
+    
     return(
-        <div> </div>
+        <Conteiner>
+            <Cabecalho>
+                    <img src="./assets/images/iconCinemonk.svg" alt=""/>
+                    <h1> CineMonk </h1>
+            </Cabecalho>
+            <Texto>
+                <p> Escolha a data que você deseja comprar o ingresso </p>
+            </Texto>
+
+
+
+        </Conteiner>
     )
 }
--- react/site/src/pages/ticketDate/styled.js
+++ react/site/src/pages/ticketDate/styled.js
@@ -1,7 +1,38 @@
 import styled from "styled-components";
 
-const containerPage = styled.div`
 
+const Conteiner = styled.body`
+        background-image: url("/assets/images/fundo-cinema.svg");
+        background-repeat: no-repeat;
+        background-size: cover;
 `
 
-export {containerPage}
+const Cabecalho = styled.header`
+
+            display: flex;
+            flex-direction: row; 
+
+        img{
+            width: 6em;
+            margin: .5em;
+        }
+
+        h1{
+            color: #f0a124;
+            font-size: 4em;
+            margin: .5em 0px;
+        }
+`
+
+const Texto = styled.article`
+
+        p{
+            color: #fff;
+            text-align: center;
+            font-size: 3em;
+        }
+`
+
+
+
+export { Cabecalho, Conteiner, Texto}
