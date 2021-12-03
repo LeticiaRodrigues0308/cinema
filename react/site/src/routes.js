9ccbd00c3f6830dd544797bf8fba3b227b564a6e
Laura Faustino <laurarfs18@gmail.com>
2021-12-03 08:55:05 -0300

    ok

--- react/site/src/routes.js
+++ react/site/src/routes.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SelectMovie from './pages/selectMovie';
 import Home from './pages/home';
 import PurchaseCompleted from './pages/purchaseCompleted/index';
-
+import ticketDate from './pages/ticketDate/index';
 
 
 export default function Routes(){
@@ -14,6 +14,7 @@ export default function Routes(){
                 <Route path="/" exact={true} component={Home}/>
                 <Route path="/select" component={SelectMovie} />
                 <Route path="/purchase-completed" component={PurchaseCompleted} />
+                <Route path="/ticketDate" component={ticketDate} />
             </Switch>
         </BrowserRouter>
     )
