44ef9a4b6a606e9b86830fbca35375cd454c27f0
João <joaopedroapparecido@gmail.com>
2021-12-05 22:03:44 -0300

    Oi rapazes, olá garotas

---
+++ react/site/src/components/bigBox/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useHistory } from "react-router";
+import { Bigbox } from "./styled";
+
+export default function Bigbox (){
+    const navigation = useHistory();
+
+    const filme = async () => {
+        navigation.push('/select')
+    }
+    return(
+        <Bigbox onClick={filme}>
+            <div className="img"> <img src="../assets/images/filme.svg" alt=""/></div>
+                <div className="box-texto">
+                    <div className="nome">Harry Potter e a Pedra Filosofal</div>
+                    <div className="box-idioma">
+                        <div className="idioma">Legendado e Dublado</div>
+                        <div className="classificacao">Classificação: +12</div>
+                    </div>
+                </div>
+        </Bigbox>
+    )
+}
--- react/site/src/pages/languageMovie/index.js
+++ react/site/src/pages/languageMovie/index.js
@@ -1,52 +1,25 @@
 import React from "react";
 import {Container} from "./styled";
+import LogoColumn from '../../components/logoColumn/index'
+import LittleBox from '../../components/littlebox'
+import Bigbox from '../../components/bigBox'
 
 export default function LanguageMovie(){
     return(
         <Container>
             <div className="cabecalho">
-                <div className="logo"><img src="./assets/images/iconCinemonk.svg" alt=""/></div>
+                <LogoColumn />
                 <div className="texto">Selecione o horário e o idioma</div>
             </div>
             <div className="conteudo">
                 <div className="filme">
-                    <div className="imagem_filme"><img src="./assets/images/" alt=""/></div>
-                    <div className="nome_filme"> Harry Potter e a Pedra Filosofal</div>
-                    <div className="duplado_filme"> Dublado e legendado</div>
-                    <div className="classificacao"> Classificação: +12 </div> 
+                    <Bigbox/>
                 </div>
-                
                 <div className="salas_horarios">
-                    <div className="box-1">
-                        <div className="sala"> Sala 8</div>
-                        <div className="horario"> 13:00 </div>
-                        <div className="dublado|legendado"> Dublado </div>
-                    </div>
-                    <div className="box-2">
-                        <div className="sala"> Sala 9</div>
-                        <div className="horario"> 13:15 </div>
-                        <div className="dublado|legendado"> Legendado </div>
-                    </div>
-                    <div className="box-3">
-                        <div className="sala"> Sala 10</div>
-                        <div className="horario"> 13:30 </div>
-                        <div className="dublado|legendado"> Dublado </div>
-                    </div>
-                    <div className="box-4">
-                        <div className="sala"> Sala 8</div>
-                        <div className="horario"> 16:15 </div>
-                        <div className="dublado|legendado"> Legendado </div>
-                    </div>
-                    <div className="box-5">
-                        <div className="sala"> Sala 9</div>
-                        <div className="horario"> 17:20 </div>
-                        <div className="dublado|legendado"> Dublado </div>
-                    </div>
-                    <div className="box-6">
-                        <div className="sala"> Sala 10</div>
-                        <div className="horario"> 18:10 </div>
-                        <div className="dublado|legendado"> Dublado </div>
-                    </div>
+                    <LittleBox/>
+                    <LittleBox/>
+                    <LittleBox/>
+                    <LittleBox/>
                 </div>
             </div>
         </Container>
